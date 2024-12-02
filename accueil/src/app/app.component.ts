a102171aa222f37f5b7c46e5915a0562f448c4ab
emarielouise972 <marielouiseesteban90@gmail.Com>
2024-12-02 12:46:02 +0100

    accueil + footer
    accueil + footer

--- accueil/src/app/app.component.ts
+++ accueil/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { PageAccueilComponent } from './page-accueil/page-accueil.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink, FooterComponent],
+  imports: [RouterOutlet,RouterLink, FooterComponent,PageAccueilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
