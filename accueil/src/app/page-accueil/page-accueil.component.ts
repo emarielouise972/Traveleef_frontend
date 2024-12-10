0e467d7553a8cdac10453db26c4a7c229156183b
emarielouise972 <marielouiseesteban90@gmail.com>
2024-12-10 14:42:05 +0100

    connexion back enoie du formulaire

--- accueil/src/app/page-accueil/page-accueil.component.ts
+++ accueil/src/app/page-accueil/page-accueil.component.ts
@@ -1,18 +1,58 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule, NgModel } from '@angular/forms';
+
 type Recommendations = {
   destinations: string,
   description: string,
   image:string
 }
+type Voyage = {
+  id_voyage : number,
+  depart : string,
+  arrivee : string,
+  date_depart : Date,
+  date_arrivee : Date,
+  prix : number,
+}
+
 @Component({
   selector: 'app-page-accueil',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor,FormsModule],
   templateUrl: './page-accueil.component.html',
   styleUrl: './page-accueil.component.css'
 })
 export class PageAccueilComponent {
+
+  formData = {
+    departure_id: '', // Code pour le départ
+    arrival_id: '', // Code pour l'arrivée
+    outbound_date: '', // Date de départ
+    return_date: '', // Date de retour
+    adults: 1, // Nombre d'adultes
+    children: 0, // Nombre d'enfants
+    animals: 0, // Nombre d'animaux
+    reduced_mobility: 0, // Nombre de personnes à mobilité réduite
+  };
+  apiUrl = 'http://localhost:5000/search'
+  constructor(private http: HttpClient) {}
+
+ submitForm() {
+    // Requête POST
+    this.http.post(this.apiUrl, this.formData).subscribe(
+      (response) => {
+        console.log('Réponse du backend :', response);
+      },
+      (error) => {
+        console.error('Erreur lors de l’envoi :', error);
+      }
+    );
+  }
+  
+  //rechercheVoyages: Voyage[] = [];
+
   recommendations = [
     {
       title: 'Maldives',
@@ -27,4 +67,10 @@ export class PageAccueilComponent {
       image: 'accueil/public/maldive.jpg',
     },
   ];
+
+  /*ngOnInit() {
+    this.http.get<Voyage[]>('http://localhost:5000/search').subscribe((data) => {
+      this.rechercheVoyages = data;
+    });
+  }*/
 }
