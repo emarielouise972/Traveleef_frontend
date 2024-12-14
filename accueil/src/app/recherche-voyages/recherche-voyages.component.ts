912a6cc81039d1a05ce52c17004cc06d7053c6e3
emarielouise972 <marielouiseesteban90@gmail.com>
2024-12-14 21:29:34 +0100

    affichage voyages

--- accueil/src/app/recherche-voyages/recherche-voyages.component.ts
+++ accueil/src/app/recherche-voyages/recherche-voyages.component.ts
@@ -1,9 +1,10 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recherche-voyages',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './recherche-voyages.component.html',
   styleUrl: './recherche-voyages.component.css'
 })
