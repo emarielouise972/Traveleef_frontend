a9212364ad1b20b4b3b02d6a336feeaffa6e2b53
fsiddiqui <117906871+Faraz1306@users.noreply.github.com>
2024-12-16 12:47:24 +0100

    mise à jour du css de détails et reservation

--- accueil/src/app/detail/detail.component.ts
+++ accueil/src/app/detail/detail.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLinkActive,
+    RouterLink
+  ],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.css'
 })
